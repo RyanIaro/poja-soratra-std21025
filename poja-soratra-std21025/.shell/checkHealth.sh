1e0e469e902c95a48b1ac0bd6b9f07b5e666c310
RyanIaro <hei.ryan.3@gmail.com>
2024-03-26 09:33:25 +0300

    fix: deleted duplicate files

--- poja-soratra-std21025/.shell/checkHealth.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install jq
-export API_URL_SSM="`aws ssm get-parameter --name /poja-soratra-std21025/$1/api/url`"
-export API_URL=`echo $API_URL_SSM | jq -r '.Parameter.Value'`
-curl --fail "$API_URL$2"
---
+++ src/main/java/school/hei/soratra/file/FileHashAlgorithm.java
@@ -0,0 +1,9 @@
+package school.hei.soratra.file;
+
+import school.hei.soratra.PojaGenerated;
+
+@PojaGenerated
+public enum FileHashAlgorithm {
+  SHA256,
+  NONE
+}
